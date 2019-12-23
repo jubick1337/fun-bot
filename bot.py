c27446db65e14c96adf90564b63e872ad21ef230
jubick1337 <mattyson.so@gmail.com>
2019-12-23 16:07:38 +0500

    first commit

---
+++ bot.py
@@ -0,0 +1,43 @@
+from emoji import emojize
+import os
+import telebot
+from predictor import Predictor
+from flask import Flask, request
+
+TOKEN = '1008454305:AAHKHXFQYphDITpwISxvDD0W-hr35Bwt05s'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+model = Predictor()
+
+
+@bot.message_handler(content_types=['text'])
+def predict_joke(message):
+    res = 'смешно' if model.predict(message.text) == 1 else 'не смешно'
+    bot.send_message(message.chat.id, 'Мне было ' + str(res))
+
+
+@bot.message_handler(commands=['help'])
+def get_help(message):
+    bot.send_message(message.chat.id, 'Попробуй меня рассмешить:)')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Привет, ' + message.from_user.first_name + " !")
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://jokes-checker.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
---
+++ predictor.py
@@ -0,0 +1,36 @@
+import keras.backend as K
+import numpy as np
+import tensorflow as tf
+import pickle
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+
+
+class Predictor:
+    def _create_model(self):
+        model = load_model('model.h5')
+        return model
+
+    def _encode_text(tokenizer, lines, length):
+        encoded = tokenizer.texts_to_sequences(lines)
+        padded = pad_sequences(encoded, maxlen=length, padding='post')
+        return padded
+
+    def _create_tokenizer(lines):
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(lines)
+        return tokenizer
+
+    def predict(self, text):
+        encoded_text = self._encode_text(self.tokenizer, text, 72)
+        p = self.model.predict(encoded_text)
+        return np.argmax(p)
+
+    def __init__(self):
+        self.sess = tf.Session()
+        self.graph = tf.get_default_graph()
+        self.model = self._create_model()
+        with open('tokenzier', 'rb') as f:
+            self.tokenizer = pickle.load(f)
+        self.model = self._create_model()
